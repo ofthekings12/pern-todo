4ec6b1cd4f1744768fcb168d50bb225837f3b604
Mikko De Los Reyes <mikkode212@gmail.com>
2022-08-31 15:32:11 -0400

    created express endpoint for update todo

--- server/index.js
+++ server/index.js
@@ -43,6 +43,16 @@ app.post("/todos", async (req, res) => {
 })
 
 // update todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {description} = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+    res.json("todo was updated")
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 //delete todo
 
