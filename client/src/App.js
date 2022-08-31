5de4509154f586cf1f4182101344fa04c4b5ead1
Mikko De Los Reyes <mikkode212@gmail.com>
2022-08-31 16:18:40 -0400

    created input todo ui, and able to input data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-
+import React, { Fragment } from "react";
 import './App.css';
+import InputTodo from "./components/InputTodo";
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+      </div>
       
-    </div>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,35 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [description, setDescription] = useState("");
+  
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:3000/todos", {method: "POST", headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(body)
+    
+    })
+    console.log(response);
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+  return  (
+  <Fragment>
+    <h1 className="text-center my-5">Input Todo</h1>
+    <form className="d-flex" onSubmit={onSubmitForm}>
+    <input type="text"
+    placeholder="add Todo"
+    className="form-control"
+    value={description}
+    onChange={e => setDescription(e.target.value)}/>
+    
+    <button className="btn btn-success">Submit</button>
+    </form>
+  </Fragment>
+  )
+}
+
+export default InputTodo;
