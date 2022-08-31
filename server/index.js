a4cbc3d5a8d265d769cfcce5bfbecf585954cf28
Mikko De Los Reyes <mikkode212@gmail.com>
2022-08-31 14:39:38 -0400

    installed nodemon and cors middleware

--- server/index.js
+++ server/index.js
@@ -5,6 +5,6 @@ const cors = require("cors");
 //middleware
 app.use(cors());
 
-app.listen(3000), () => {
-  console.log("Sever is running on port 5000");
-}
+app.listen(3000, () => {
+  console.log("Server is running on port 5000");
+});
