e5259347a58742e1a74fc8cfead28eb76e61a47b
Mikko De Los Reyes <mikkode212@gmail.com>
2022-09-01 13:42:13 -0400

    created a delete button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,54 +1,49 @@
-import React, { Fragment, useState, useEffect} from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 export default function ListTodos() {
-
-const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   async function getTodos() {
-    const res = await fetch ("http://localhost:3000/todos");
+    const res = await fetch("http://localhost:3000/todos");
 
     const todoArray = await res.json();
     setTodos(todoArray);
   }
-useEffect(() => {
-  getTodos();
-}, [])
+  useEffect(() => {
+    getTodos();
+  }, []);
 
-console.log(todos)
+  console.log(todos);
 
   return (
     <Fragment>
       {" "}
       <table className="table mt-5">
-  <thead>
-    <tr>
-      <th scope="col">Description</th>
-      <th scope="col">Edit</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    {/* <tr>
+        <thead>
+          <tr>
+            <th scope="col">Description</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* <tr>
       <th scope="row">1</th>
       <td>Mark</td>
       <td>Otto</td>
       <td>@mdo</td>
     </tr> */}
-    {todos.map(todo => (
-      <tr>
-        <td>
-          {todo.description}
-        </td>
-        <td>
-          Edit
-        </td>
-        <td>
-          Delete
-        </td>
-      </tr>
-    ))}
-  </tbody>
-</table>
+          {todos.map((todo) => (
+            <tr>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>
+                <button className="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Fragment>
-  )
+  );
 }
